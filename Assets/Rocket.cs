ba55eff75846d2e8d56d139feac9a2eeae6fd977
Mika Vesasto <mika.vesasto@saunalahti.fi>
2020-05-22 23:08:27 +0300

    Resolve movement bugs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,36 +1,62 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
+
+    AudioSource audioSource;
+    bool play;
+    bool toggleChange;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
+        
     }
 
-    private void ProcessInput()
+
+
+    private void Thrust()
     {
-       if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+
+                audioSource.Play();
+            }
         }
-       
-        if (Input.GetKey(KeyCode.A)) {
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // Take manual control of rotation
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.forward);
         }
-        else if (Input.GetKey(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(-Vector3.forward);
         }
+        rigidbody.freezeRotation = false; // Resume physics control of rotation
     }
 }
